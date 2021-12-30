bde9ea747f65c41c7836f8cc1b4d921adb3f9072
norwegianblackmetal <finojenok.m@gmail.com>
2021-12-30 19:25:11 +0300

    .

--- sql_queries/insert_values.sql
+++ sql_queries/insert_values.sql
@@ -1,22 +1,22 @@
-insert into daydash.Day(DayName) values ("Monday")
-insert into daydash.Day(DayName) values ("Tuesday")
-insert into daydash.Day(DayName) values ("Wednesday")
-insert into daydash.Day(DayName) values ("Thursday")
-insert into daydash.Day(DayName) values ("Friday")
-insert into daydash.Day(DayName) values ("Saturday")
-insert into daydash.Day(DayName) values ("Sunday")
+insert into daydash.Day(DayName) values ("Monday");
+insert into daydash.Day(DayName) values ("Tuesday");
+insert into daydash.Day(DayName) values ("Wednesday");
+insert into daydash.Day(DayName) values ("Thursday");
+insert into daydash.Day(DayName) values ("Friday");
+insert into daydash.Day(DayName) values ("Saturday");
+insert into daydash.Day(DayName) values ("Sunday");
 
-insert into daydash.Routine(RoutineName) values ("Завтрак")
-insert into daydash.Routine(RoutineName) values ("Чтение новостей")
-insert into daydash.Routine(RoutineName) values ("Работа")
-insert into daydash.Routine(RoutineName) values ("Обед")
-insert into daydash.Routine(RoutineName) values ("Работа")
-insert into daydash.Routine(RoutineName) values ("Бег")
-insert into daydash.Routine(RoutineName) values ("Турник")
-insert into daydash.Routine(RoutineName) values ("Родословная")
-insert into daydash.Routine(RoutineName) values ("Финансы")
-insert into daydash.Routine(RoutineName) values ("Текущие")
-insert into daydash.Routine(RoutineName) values ("Планирование")
-insert into daydash.Routine(RoutineName) values ("Linux")
-insert into daydash.Routine(RoutineName) values ("Чтение книг")
-insert into daydash.Routine(RoutineName) values ("Социализация")
+insert into daydash.Routine(RoutineName) values ("Завтрак");
+insert into daydash.Routine(RoutineName) values ("Чтение новостей");
+insert into daydash.Routine(RoutineName) values ("Работа");
+insert into daydash.Routine(RoutineName) values ("Обед");
+insert into daydash.Routine(RoutineName) values ("Работа");
+insert into daydash.Routine(RoutineName) values ("Бег");
+insert into daydash.Routine(RoutineName) values ("Турник");
+insert into daydash.Routine(RoutineName) values ("Родословная");
+insert into daydash.Routine(RoutineName) values ("Финансы");
+insert into daydash.Routine(RoutineName) values ("Текущие");
+insert into daydash.Routine(RoutineName) values ("Планирование");
+insert into daydash.Routine(RoutineName) values ("Linux");
+insert into daydash.Routine(RoutineName) values ("Чтение книг");
+insert into daydash.Routine(RoutineName) values ("Социализация");
