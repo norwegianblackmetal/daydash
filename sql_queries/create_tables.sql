dc9596db889b4776441b30f920875662fb26e5ce
norwegianblackmetal <finojenok.m@gmail.com>
2021-12-30 18:44:17 +0300

    .

--- sql_queries/create_tables.sql
+++ sql_queries/create_tables.sql
@@ -2,16 +2,16 @@ create table daydash.Day
 (
 	DayId int primary key,
 	DayName nvarchar(128) not null
-)
+);
 
 create table daydash.Routine
 (
 	RoutineId int primary key,
 	RoutineName nvarchar(128) not null
-)
+);
 
 create table daydash.DayRoutineLink
 (
 	DayId int foreign key references daydash.Day(DayId),
 	RoutineId int foreign key references daydash.Routine(RoutineId)
-)
+);
